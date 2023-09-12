b5d00d734406f74d7d28f029bc47031c91c250eb
JeongSeongHyeon <43773374+GriTRini@users.noreply.github.com>
2023-09-12 15:59:18 +0900

    이기종 코드 추가

---
+++ offboard_node/src/swarm_heterogeneous.cpp
@@ -0,0 +1,193 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+mavros_msgs::State uav0_current_state;
+mavros_msgs::State uav1_current_state;
+mavros_msgs::State uav2_current_state;
+
+geometry_msgs::PoseStamped leader_pose;
+
+double leader_heading = 0.0;
+double leader_rolling = 0.0;
+double leader_pitching = 0.0;
+bool take_down_flag = false;
+
+void uav0_state_now(const mavros_msgs::State::ConstPtr& msg){
+    uav0_current_state = *msg;
+}
+
+void uav1_state_now(const mavros_msgs::State::ConstPtr& msg){
+    uav1_current_state = *msg;
+}
+
+void uav2_state_now(const mavros_msgs::State::ConstPtr& msg){
+    uav2_current_state = *msg;
+}
+
+void goal_pose(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+    leader_pose = *msg;
+    tf2::Quaternion tf_quaternion;
+    tf_quaternion.setX(leader_pose.pose.orientation.x);
+    tf_quaternion.setY(leader_pose.pose.orientation.y);
+    tf_quaternion.setZ(leader_pose.pose.orientation.z);
+    tf_quaternion.setW(leader_pose.pose.orientation.w);
+
+    tf2::Matrix3x3 mat(tf_quaternion);
+
+    double roll, pitch, yaw;
+    mat.getRPY(roll, pitch, yaw);
+    leader_rolling = roll;
+    leader_pitching = pitch;
+    leader_heading = yaw;
+}
+
+bool setOffboardMode(ros::ServiceClient& set_mode_client) {
+    mavros_msgs::SetMode offboard_set_mode;
+    offboard_set_mode.request.custom_mode = "OFFBOARD";
+    if (set_mode_client.call(offboard_set_mode) && offboard_set_mode.response.mode_sent) {
+        return true;
+    }
+    return false;
+}
+
+bool armVehicle(ros::ServiceClient& arming_client) {
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+    if (arming_client.call(arm_cmd) && arm_cmd.response.success) {
+        return true;
+    }
+    return false;
+}
+
+bool setReturnMode(ros::ServiceClient& set_mode_client) {
+    mavros_msgs::SetMode return_set_mode;
+    return_set_mode.request.custom_mode = "AUTO.RTL";
+    if (set_mode_client.call(return_set_mode) && return_set_mode.response.mode_sent) {
+        return true;
+    }
+    return false;
+}
+
+
+void swarm_setpoint(ros::Publisher& local_pos_pub, ros::Rate& rate, double offset_x, double offset_y, double offset_z, double min_height, bool heading) {
+    geometry_msgs::PoseStamped pose;
+
+    double l = sqrt(pow(offset_x, 2) + pow(offset_y, 2)); 
+
+    double radian = atan2(offset_y, offset_x);
+
+    pose.pose.position.x = leader_pose.pose.position.x + (l * sin(radian - leader_heading));
+    pose.pose.position.y = leader_pose.pose.position.y + (l * cos(radian - leader_heading));
+    pose.pose.position.z = leader_pose.pose.position.z + offset_z;
+
+    if (heading == true) {
+        pose.pose.orientation.x = leader_pose.pose.orientation.x;
+        pose.pose.orientation.y = leader_pose.pose.orientation.y;
+        pose.pose.orientation.z = leader_pose.pose.orientation.z;
+        pose.pose.orientation.w = leader_pose.pose.orientation.w;
+
+    }
+
+    if (uav0_current_state.armed) {
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "swarm_heterogeneous");
+    ros::NodeHandle nh;
+
+    ros::Subscriber leader_pos_sub = nh.subscribe<geometry_msgs::PoseStamped> ("uav0/mavros1/local_position/pose", 10, goal_pose);
+    
+    ros::Subscriber uav0_state_sub = nh.subscribe<mavros_msgs::State> ("uav0/mavros1/state", 10, uav0_state_now);
+    ros::Subscriber uav1_state_sub = nh.subscribe<mavros_msgs::State> ("uav1/mavros1/state", 10, uav1_state_now);
+    ros::Subscriber uav2_state_sub = nh.subscribe<mavros_msgs::State> ("uav2/mavros1/state", 10, uav2_state_now);
+
+    ros::Publisher uav1_pos_pub = nh.advertise<geometry_msgs::PoseStamped> ("uav1/mavros1/setpoint_position/local", 10);
+    ros::Publisher uav2_pos_pub = nh.advertise<geometry_msgs::PoseStamped> ("uav2/mavros1/setpoint_position/local", 10);
+
+    ros::ServiceClient uav1_arming_client = nh.serviceClient<mavros_msgs::CommandBool> ("uav1/mavros1/cmd/arming");
+    ros::ServiceClient uav2_arming_client = nh.serviceClient<mavros_msgs::CommandBool> ("uav2/mavros1/cmd/arming");
+
+    ros::ServiceClient uav1_set_mode_client = nh.serviceClient<mavros_msgs::SetMode> ("uav1/mavros1/set_mode");
+    ros::ServiceClient uav2_set_mode_client = nh.serviceClient<mavros_msgs::SetMode> ("uav2/mavros1/set_mode");
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(100.0);
+    ros::Time last_request = ros::Time::now();
+
+    while(ros::ok()){
+        if (std::abs(leader_pitching) >= 0.785398 || std::abs(leader_rolling) >= 0.785398) {
+            take_down_flag = true;
+        }       
+        if (ros::ok() && uav1_current_state.connected) {
+            if (uav0_current_state.mode == "AUTO.RTL" || uav0_current_state.mode == "AUTO.LAND" || take_down_flag == 1) {
+                setReturnMode(uav1_set_mode_client);
+            } else{
+                if( uav1_current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(0.5))){
+                    if (setOffboardMode(uav1_set_mode_client)) {
+                        ROS_INFO("uav1_Offboard enabled");
+                        last_request = ros::Time::now();
+                    }
+                } else {
+                    if( !uav1_current_state.armed && (ros::Time::now() - last_request > ros::Duration(0.5))){
+                        if (armVehicle(uav1_arming_client)) {
+                            ROS_INFO("uav1_Vehicle armed");
+                            last_request = ros::Time::now();
+                        }
+                    }
+                }
+                swarm_setpoint(uav1_pos_pub, rate, -5, -5, 10, 2, true);
+                ros::spinOnce();
+                rate.sleep();  
+            }
+            
+        }else {
+            ros::spinOnce();
+            rate.sleep();
+        }
+
+        if (ros::ok() && uav2_current_state.connected) {
+            if (uav0_current_state.mode == "AUTO.RTL" || uav0_current_state.mode == "AUTO.LAND" || take_down_flag == 1) {
+                setReturnMode(uav2_set_mode_client);
+            } else{
+                if( uav2_current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(0.5))){
+                    if (setOffboardMode(uav2_set_mode_client)) {
+                        ROS_INFO("uav2_Offboard enabled");
+                        last_request = ros::Time::now();
+                    }
+                    } else {
+                        if( !uav2_current_state.armed && (ros::Time::now() - last_request > ros::Duration(0.5))){
+                            if (armVehicle(uav2_arming_client)) {
+                                ROS_INFO("uav2_Vehicle armed");
+                                last_request = ros::Time::now();
+                            }
+                        }
+                    }
+                swarm_setpoint(uav2_pos_pub, rate,  5, -5, 10, 2, false);
+                ros::spinOnce();
+                rate.sleep();
+            }
+            
+        }else {
+            ros::spinOnce();
+            rate.sleep();
+        }
+    }
+    return 0;
+}
